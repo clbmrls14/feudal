d2041e57b6884050cfa30d23a06dc8e56293c328
Caleb Morales <caleb.morales@ad.snow.edu>
2019-11-11 11:39:36 -0700

    Built a .vue file to work with for the board

---
+++ game_board.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="board">
+        <div class="board-tile board-a2"><img :src="tile_img"></div>
+        <div class="board-tile board-a3"><img :src="tile_img"></div>
+        <div class="board-tile board-a1"><img :src="tile_img"></div>
+        <div class="board-tile board-b1"><img :src="tile_img"></div>
+        <div class="board-tile board-b2"><img :src="tile_img"></div>
+        <div class="board-tile board-b3"><img :src="tile_img"></div>
+        <div class="board-tile board-c1"><img :src="tile_img"></div>
+        <div class="board-tile board-c2"><img :src="tile_img"></div>
+        <div class="board-tile board-c3"><img :src="tile_img"></div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    var game_board = new Vue({
+        el: '.board',
+        data: {
+            board_height: 3,
+            board_width: 3,
+            tile: {
+                tile_name: 'Field',
+                tile_img: '../img/empty-tile.png'
+            }
+        }
+    });
+</script>
+
+<style scoped>
+    div.board {
+        display: grid;
+        grid-template-columns: auto auto auto;
+        grid-template-rows: auto auto auto;
+        grid-template-areas:
+            'a1 a2 a3'
+            'b1 b2 b3'
+            'c1 c2 c3'
+        ;
+        grid-gap: 10px;
+        justify-content: center;
+    }
+</style>
