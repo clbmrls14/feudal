c2e702da44d0b838fdcb45a07cf2eacf044cf637
Jaaron Nielsen <nielsenjaaron@gmail.com>
2019-11-13 19:59:26 -0700

    more changes

--- src/data/tile.js
+++ src/data/tile.js
@@ -1,4 +1,4 @@
-const images = require.context('../assets/img', true, /\.png$/);
+const images = require.context('./img', true, /\.png$/);
 
 const tiles = {
     tile: [
@@ -6,19 +6,26 @@ const tiles = {
             id: 0,
             description: 'All figures can move on field tiles.',
             title: 'Field',
-            src: images('./tile_field.png')
+            src: images('./tile_field.png'),
+            mountain: false,
+            swamp: false
+
         },
         {
             id: 1,
             description: 'Mounted figures may not move through the swamp.',
             title: 'Swamp',
-            src: images('./swamp.png')
+            src: images('./tile_swamp.png'),
+            mountain: false,
+            swamp: true
         },
         {
             id: 2,
             description: 'No characters may move through the mountain.',
             title: 'Mountain',
-            src: images('./mountain.png')
+            src: images('./tile_mountain.png'),
+            mountain: true,
+            swamp: false
         }
     ]
 };
