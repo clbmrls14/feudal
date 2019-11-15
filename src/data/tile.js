3135c69adadf80273d1603de76e8cc493589ccc4
Caleb Morales <caleb.morales@ad.snow.edu>
2019-11-14 18:07:29 -0700

    trying to fix things

--- src/data/tile.js
+++ src/data/tile.js
@@ -97,3 +97,12 @@ const game = {
 };
 
 export default game;
+// import tiles from './tile.js';
+
+// const tile_board = [
+//     [tiles.tile[0], tiles.tile[0], tiles.tile[1]],
+//     [tiles.tile[0], tiles.tile[2], tiles.tile[0]],
+//     [tiles.tile[0], tiles.tile[1], tiles.tile[0]]
+// ];
+
+// export default tile_board;
