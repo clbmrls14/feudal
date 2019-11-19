82f689f244fb9fb170e9dbc43fb3c62a1b30554e
Caleb Morales <caleb.morales@ad.snow.edu>
2019-11-19 13:06:53 -0700

    blah

---
+++ src/data/tile_board.js
@@ -0,0 +1,9 @@
+import tiles from './tile.js';
+
+const tile_board = [
+    [tiles.tile[0], tiles.tile[0], tiles.tile[1]],
+    [tiles.tile[0], tiles.tile[2], tiles.tile[0]],
+    [tiles.tile[0], tiles.tile[1], tiles.tile[0]]
+];
+
+export default tile_board;
